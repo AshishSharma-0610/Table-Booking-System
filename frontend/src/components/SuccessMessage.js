4e8e0e8b40f1bf742e91af9f8086a324916447f9
AshishSharma-0610 <136923510+AshishSharma-0610@users.noreply.github.com>
2025-01-02 16:27:03 +0530

    Delete frontend directory

--- frontend/src/components/SuccessMessage.js
+++
@@ -1,9 +0,0 @@
-// src/components/SuccessMessage.js
-
-export default function SuccessMessage({ message, type }) {
-    return (
-        <div className={`message ${type}`}>
-            <p>{message}</p>
-        </div>
-    );
-}
